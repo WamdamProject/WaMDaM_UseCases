e57128a7db77d6b7aa658865efc03741e399a70f
Adel M. Abdallah <amabdallah@aggiemail.usu.edu>
2018-01-18 12:59:55 -0700

    update Mapping to Mappings
    Change YearType

--- UseCases_files/4Queries_SQL/2.2Identify_aggregate_TimeSeriesValues.sql
+++ UseCases_files/4Queries_SQL/2.2Identify_aggregate_TimeSeriesValues.sql
@@ -25,16 +25,16 @@ Updated October 30, 2017
 
                                                    --The first SELECT statement (get the daily data and convert to monthly)
 
---DatasetAcronym,AttributeName, InstanceName,AggregationStatisticCV,IntervalTimeUnitCV,UnitNameCV,WaterOrCalendarYear,YearMonth, CountDays,CalenderYear,CumulativeMonthly
+--DatasetAcronym,AttributeName, InstanceName,AggregationStatisticCV,IntervalTimeUnitCV,UnitNameCV,YearType,YearMonth, CountDays,CalenderYear,CumulativeMonthly
 
 SELECT DatasetAcronym,AttributeName, InstanceName,AggregationStatisticCV,IntervalTimeUnitCV,UnitNameCV,
-WaterOrCalendarYear,strftime('%m/%Y', DateTimeStamp) as YearMonth, count(value) As CountDays,
+YearType,strftime('%m/%Y', DateTimeStamp) as YearMonth, count(value) As CountDays,
 --convert the time stamp to be in the format of Month and Year (no days)
 
 
---check if it is a water year by quering the field "WaterOrCalendarYear" in the TimeSeries table
+--check if it is a water year by querying the field "YearType" in the TimeSeries table
 Case 
-        WHEN WaterOrCalendarYear='WaterYear' AND (strftime('%m', DateTimeStamp) ='10' or  strftime('%m', DateTimeStamp) ='11' or  strftime('%m', DateTimeStamp) ='12')
+        WHEN YearType='WaterYear' AND (strftime('%m', DateTimeStamp) ='10' or  strftime('%m', DateTimeStamp) ='11' or  strftime('%m', DateTimeStamp) ='12')
         -- if it is a water year, then subtract one year from the time stamps of Oct., Nov., and Dec. months in each year
         THEN date(DateTimeStamp,'-1 year')   
         --if not a water year, then keep the time stamp as is
@@ -60,20 +60,20 @@ ON "ObjectTypes"."DatasetID"="Datasets"."DatasetID"
 LEFT JOIN  "Attributes"
 ON "Attributes"."ObjectTypeID"="ObjectTypes"."ObjectTypeID"
 
-LEFT JOIN "Mapping"
-ON "Mapping"."AttributeID"= "Attributes"."AttributeID"
+LEFT JOIN "Mappings"
+ON "Mappings"."AttributeID"= "Attributes"."AttributeID"
 
 LEFT JOIN "Instances" 
-ON "Instances"."InstanceID"="Mapping"."InstanceID"
+ON "Instances"."InstanceID"="Mappings"."InstanceID"
 
 LEFT JOIN "DataValuesMapper" 
-ON "DataValuesMapper"."DataValuesMapperID"="Mapping"."DataValuesMapperID"
+ON "DataValuesMapper"."DataValuesMapperID"="Mappings"."DataValuesMapperID"
 
-LEFT JOIN "ScenarioMapping"
-ON "ScenarioMapping"."MappingID"="Mapping"."MappingID"
+LEFT JOIN "ScenarioMappings"
+ON "ScenarioMappings"."MappingID"="Mappings"."MappingID"
 
 LEFT JOIN "Scenarios" 
-ON "Scenarios"."ScenarioID"="ScenarioMapping"."ScenarioID"
+ON "Scenarios"."ScenarioID"="ScenarioMappings"."ScenarioID"
 
 LEFT JOIN "MasterNetworks" 
 ON "MasterNetworks"."MasterNetworkID"="Scenarios"."MasterNetworkID"
@@ -99,7 +99,7 @@ AND "AttributeNameCV"='Flow'
 -- Daily time series. 
 AND IntervalTimeUnitCV='day'
 
-GROUP BY DatasetAcronym,AttributeName,InstanceName,WaterOrCalendarYear,YearMonth
+GROUP BY DatasetAcronym,AttributeName,InstanceName,YearType,YearMonth
 
 -- use this only if converting from daily to monthly (otherwise, months wont show up because their count is just 1)
 --The use of "HAVING" clause enables you to specify conditions that filter which group results appear in the final results.
@@ -117,14 +117,14 @@ Having CountDays>=27
                                                    --The second SELECT statement (get the monthly data and keep it monthly)
 
 SELECT DatasetAcronym,AttributeName, InstanceName,AggregationStatisticCV,IntervalTimeUnitCV,UnitNameCV,
-WaterOrCalendarYear,strftime('%m/%Y', DateTimeStamp) as YearMonth, count(value) As CountDays,
+YearType,strftime('%m/%Y', DateTimeStamp) as YearMonth, count(value) As CountDays,
 
 
---check if it is a water year by querying the field "WaterOrCalendarYear" in the TimeSeries table
+--check if it is a water year by querying the field "YearType" in the TimeSeries table
 --convert the time stamp to be in the format of Month and Year (no days)
 -- If the time series is "WateYear", then convert it to a calendar year.
 Case 
-        WHEN WaterOrCalendarYear='WaterYear' AND (strftime('%m', DateTimeStamp) ='10' or  strftime('%m', DateTimeStamp) ='11' or  strftime('%m', DateTimeStamp) ='12')
+        WHEN YearType='WaterYear' AND (strftime('%m', DateTimeStamp) ='10' or  strftime('%m', DateTimeStamp) ='11' or  strftime('%m', DateTimeStamp) ='12')
         -- if it is a water year, then subtract one year from the time stamps of Oct., Nov., and Dec. months in each year
         THEN date(DateTimeStamp,'-1 year')   
         --if not a water year, then keep the time stamp as is
@@ -151,20 +151,20 @@ ON "ObjectTypes"."DatasetID"="Datasets"."DatasetID"
 LEFT JOIN  "Attributes"
 ON "Attributes"."ObjectTypeID"="ObjectTypes"."ObjectTypeID"
 
-LEFT JOIN "Mapping"
-ON "Mapping"."AttributeID"= "Attributes"."AttributeID"
+LEFT JOIN "Mappings"
+ON "Mappings"."AttributeID"= "Attributes"."AttributeID"
 
 LEFT JOIN "Instances" 
-ON "Instances"."InstanceID"="Mapping"."InstanceID"
+ON "Instances"."InstanceID"="Mappings"."InstanceID"
 
 LEFT JOIN "DataValuesMapper" 
-ON "DataValuesMapper"."DataValuesMapperID"="Mapping"."DataValuesMapperID"
+ON "DataValuesMapper"."DataValuesMapperID"="Mappings"."DataValuesMapperID"
 
-LEFT JOIN "ScenarioMapping"
-ON "ScenarioMapping"."MappingID"="Mapping"."MappingID"
+LEFT JOIN "ScenarioMappings"
+ON "ScenarioMappings"."MappingID"="Mappings"."MappingID"
 
 LEFT JOIN "Scenarios" 
-ON "Scenarios"."ScenarioID"="ScenarioMapping"."ScenarioID"
+ON "Scenarios"."ScenarioID"="ScenarioMappings"."ScenarioID"
 
 LEFT JOIN "MasterNetworks" 
 ON "MasterNetworks"."MasterNetworkID"="Scenarios"."MasterNetworkID"
@@ -191,7 +191,7 @@ AND "AttributeNameCV"='Flow'
 --Then you can use the “Having” clause below for daily but need to comment it for monthly
 AND IntervalTimeUnitCV='month'
 
-GROUP BY DatasetAcronym,AttributeName,InstanceName,WaterOrCalendarYear,YearMonth
+GROUP BY DatasetAcronym,AttributeName,InstanceName,YearType,YearMonth
 
 -- use this only if converting from daily to monthly (otherwise, months wont show up because their count is just 1)
 --The use of "HAVING" clause enables you to specify conditions that filter which group results appear in the final results.
