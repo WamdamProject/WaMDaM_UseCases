dcd27c5a6e1f28b7737196f281119096cfd7cc48
Adel M. Abdallah <amabdallah@aggiemail.usu.edu>
2018-03-29 14:37:56 -0600

    Update 2.2bIdentify_aggregate_TimeSeriesValues.py

--- UseCases_files/6Figures_Python/2.2bIdentify_aggregate_TimeSeriesValues.py
+++ UseCases_files/6Figures_Python/2.2bIdentify_aggregate_TimeSeriesValues.py
@@ -1,8 +1,8 @@
-# Use Case 2.2bIdentify_aggregate_TimeSeriesValues.py
+# Use Case 2.2Identify_aggregate_TimeSeriesValues.csv
 # plot aggregated to monthly and converted to acre-feet time series data of multiple sources
 
 # Adel Abdallah
-# November 16, 2017
+# March 29, 2018
 
 import plotly
 import plotly.plotly as py
@@ -12,7 +12,8 @@ from random import randint
 import pandas as pd
 
 ## read the input data from GitHub csv file which is a direct query output for this  query:
-# 3.2Identify_aggregate_TimeSeriesValues.sql
+# 2.2Identify_aggregate_TimeSeriesValues.csv
+
 df = pd.read_csv("https://raw.githubusercontent.com/WamdamProject/WaMDaM_UseCases/master/UseCases_files/5Results_CSV/2.2Identify_aggregate_TimeSeriesValues.csv")
 
 # identify the data for four time series only based on the DatasetAcronym column header 
@@ -21,40 +22,32 @@ subsets = df.groupby(column_name)
 data = []
 
 # for each subset (curve), set up its legend and line info manually so they can be edited
-
 subsets_settings = {
     'UDWRFlowData': {
-        'symbol': "star",
+        'dash': 'solid',
         'legend_index': 0,
         'legend_name': 'Utah Division of Water Res.',
-        'width':'2',
-        'size' :'7',
-        'color':'rgb(153, 15, 15)',
-        'mode': 'lines+markers'
-        },
-    'CUHASI': {
-        'symbol': "square",
-        'legend_index': 1,
-         'size' :'10',
-        'legend_name': 'CUAHSI',
         'width':'3',
-        'color':'rgb(15, 107, 153)',
-        'show_legend': False,
+        'color':'rgb(153, 15, 15)'
+        },
+    'CUAHSI': {
+        'dash': 'dash',
+        'legend_index': 1,
+        'legend_name': 'USGS',
+        'width':'4',
+        'color':'rgb(15, 107, 153)'
         },
     'IdahoWRA': {
-        'symbol': "triangle-down",
+        'dash': 'soild',
         'legend_index': 2,
-         'size' :'6',
         'legend_name': 'Idaho Department of Water Res.',
         'width':'3',
         'color':'rgb(38, 15, 153)'
         },    
-    'BearRiverCommission': { # this one is the name of subset as it appears in the csv file
-        'symbol': "106",     # this is property of the line (curve)
-                'size' :'6',
-
+    'BearRiverCommission': { # this oone is the name of subset as it appears in the csv file
+        'dash': 'dot',     # this is properity of the line (curve)
         'legend_index': 3,   # to order the legend
-        'legend_name': "Bear River Commission",  # this is the manual curve name 
+        'legend_name': 'Bear River Commission',  # this is the manual curve name 
          'width':'4',
         'color':'rgb(107, 153, 15)'
         }
@@ -68,39 +61,23 @@ for subset in subsets.groups.keys():
     print subset
     dt = subsets.get_group(name=subset)
     s = go.Scatter(
-        x=dt.CalenderYear.map(lambda z: str(z)[:-3]),
-        y=dt['CumulativeMonthly'],
-        name = subsets_settings[subset]['legend_name'],       
-        opacity = 1,
-        
-        # Get mode from settings dictionary, if there is no mode
-        # defined in dictinoary, then default is markers.
-        mode = subsets_settings[subset].get('mode', 'markers'),
-        
-        # Get legend mode from settings dictionary, if there is no mode
-        # defined in dictinoary, then default is to show item in legend.
-        showlegend = subsets_settings[subset].get('show_legend', True),
-        
-        marker = dict(
-            size =subsets_settings[subset]['size'],
-            color = '#FFFFFF',      # white
-            symbol =subsets_settings[subset]['symbol'],
-            line = dict(
-                color =subsets_settings[subset]['color'],
-                width =subsets_settings[subset]['width'], 
-                ),
-            ),
-            
-        line = dict(
-            color =subsets_settings[subset]['color'],
-            width =subsets_settings[subset]['width'], 
-            ),
-        )
-    
+                    x=dt.CalenderYear.map(lambda z: str(z)[:-3]),
+                    y=dt['CumulativeMonthly'],
+                    name = subsets_settings[subset]['legend_name'],
+                    line = dict(
+                        color =subsets_settings[subset]['color'],
+                        width =subsets_settings[subset]['width'], 
+                        dash=subsets_settings[subset]['dash']
+                               ),
+                        opacity = 1                                
+                  )
     data.append(s)
+
+    
+    
     
 # Legend is ordered based on data, so we are sorting the data based 
-# on desired legend order indicated by the index value entered above
+# on desired legend order indicarted by the index value entered above
 data.sort(key=lambda x: subsets_settings[subsets_names[x['name']]]['legend_index'])
 
 # set up the figure layout parameters
@@ -115,26 +92,26 @@ layout = dict(
          ticklen=15,
          #zerolinewidth=4,
          zerolinecolor='#00000',
-         range = ['0', '6000'],
-         dtick=1000,
+
+         dtick=30000,
                  ),
     xaxis = dict(
          #title = "Time <br> (month/year)",
          #autotick=False,
-        tick0='1994-01-01',
-        showline=True,
-        dtick='M12',
-        ticks='outside',
+        tick0='1900-01-01',
+        dtick='M180',
+        ticks='inside',
         tickwidth=0.5,
         #zerolinewidth=4,
         ticklen=27,
-        #zerolinecolor='#00000',
+        zerolinecolor='#00000',
         tickcolor='#000',
         tickformat= "%Y",
-        range = ['1994', '2000']
+       range = ['1920', '2020']
+
                 ),
     legend=dict(
-        x=0.3,y=1,
+        x=0.1,y=1.2,
         bordercolor='#00000',
             borderwidth=2
 
@@ -150,14 +127,13 @@ layout = dict(
     
     font=dict( size=35)
              )
-             
 # create the figure object            
 fig = dict(data=data, layout=layout)
 
 # plot the figure 
-#py.iplot(fig, filename = "2.2bIdentify_aggregate_TimeSeriesValues")       
+#py.iplot(fig, filename = "2.2Identify_aggregate_TimeSeriesValues")       
 
 
 ## it can be run from the local machine on Pycharm like this like below
 ## It would also work here offline but in a seperate window  
-plotly.offline.plot(fig, filename = "2.2bIdentify_aggregate_TimeSeriesValues")       
+plotly.offline.plot(fig, filename = "2.2Identify_aggregate_TimeSeriesValues")       
